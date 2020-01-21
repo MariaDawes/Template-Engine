bed27003449441a5d456e8fd695b040be616fdee
MariaDawes <maria.c.dawes@gmail.com>
2020-01-21 09:44:02 -0600

    added new files

---
+++ app.js
@@ -0,0 +1,189 @@
+const inquirer = require("inquirer");
+//const Employee = require("./employee");
+//const Manager = require("./manager");
+//const Engineer = require("./engineer");
+//const Intern = require("./intern");
+// const generateHTML = require("./generateHTML");
+
+
+
+
+function init() {
+
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is the employee's name?",
+                validate: validateName
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the employee's id number?",
+                validate: validateNumber
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the employee's email?",
+                validate: validateEmail
+            },
+            {
+                type: "list",
+                name: "title",
+                message: "What is the employee's title?",
+                choices: ["manager", "engineer", "intern"]
+            }
+           
+        ])
+        .then(ans => {
+            
+            console.log("name:", ans.name);
+            console.log("id:", ans.id);
+            console.log("email:", ans.email);
+            console.log("title:", ans.title);
+
+        
+            if (ans.title == "manager") {
+
+                    inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            name: "officenumber",
+                            message: "What is the employee's office number?",
+                            validate: validateNumber
+                        }
+                    ])
+                    .then(ans1 => {
+                        console.log("office number:", ans1.officenumber);
+                        anotherEmployee();
+                    })
+            } else if (ans.title == "engineer") {
+
+                    inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            name: "githubprofile",
+                            message: "What is the employee's GitHub profile?",
+                            validate: validateGithub
+                        }
+                    ])
+                    .then(ans2 => {
+                        console.log("Git hub profile:", ans2.githubprofile);
+                        anotherEmployee();
+                    })
+                
+            } else if (ans.title == "intern") {
+
+                    inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            name: "school",
+                            message: "What is the employee's school name?",
+                            validate: validateName
+                        }
+                    ])
+                    .then(ans3 => {
+                        console.log("Git hub profile:", ans3.school);
+                        anotherEmployee();
+                    })
+
+            } // end of ifs
+                    
+        }) // end of .then
+
+        // Input validation functions    
+        function validateName(input) {
+            if (!input.match(/^[A-Z][A-Z ]{0,}/i)) {
+                return "Must contain at least 1 character and may contain only letters and spaces!"; 
+            } else {
+                return true;
+            }
+        }
+        function validateNumber(input) {
+            if (!input.match(/^[0-9]+$/)) {
+            return "Must be a number!";
+            } else {
+            return true;
+            }
+        }
+        function validateEmail(input) {
+            if (!input.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i)) {
+            return "Must be a valid email address!";
+            } else {
+            return true;
+            }
+        }
+        function validateGithub(input) {
+            if (!input.match(/^[A-Z0-9_]{3,}$/i)) {
+            return "Must be a valid Github name! (3 characters - numbers and/or letters)";
+            } else {
+            return true;
+        }
+        }
+        // end of Input validation functions 
+
+        function anotherEmployee() {
+            console.log("Another employee");
+
+            inquirer
+            .prompt([
+                {
+                    type: "list",
+                    name: "another",
+                    message: "Do you want to add another employee?",
+                    choices: ["Yes", "No"] 
+                }
+            ])
+            .then(ans4 => {
+                console.log("Yes or No?", ans4.another);
+                if (ans4.another == "Yes") {
+                    storeData();
+                    init();    
+                } 
+                else {
+                console.log("Your html will be ready soon!");
+                storeData();
+                doHtml();
+                }
+            })
+        }
+
+        function storeData() {
+            console.log("Store data!");
+        }
+       
+        function doHtml() {
+            console.log("Do html!");
+        }
+
+
+
+                    
+//                     // Get the response axios sent and set varibles to support pdf file generation
+//                     var color = answers.color
+//                     var starsUsers = 0
+//                     var filehtml = generateHTML({ ...answers, starsUsers, ...response.data })
+//                     const options = { format: 'Letter' };
+
+//                     // Generate pdf file
+//                     pdf.create(filehtml, options).toFile('./generateHTML.pdf', function (err, res) {
+//                         if (err) return console.log(err);
+//                         console.log(res);
+//                     })
+//                 })
+//                 .catch(function (error) {
+//                     // handle error
+//                     console.log(error);
+//                 })
+
+
+        
+} // end function init
+
+init();
